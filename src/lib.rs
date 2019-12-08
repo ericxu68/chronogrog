d1f12bb8bedae39868eb00334d655383462c7ae0
Scott Johnson <jaywir3@gmail.com>
2019-12-08 10:39:30 -0600

    :sparkles: Add Recipes to chronogrog.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,76 @@
+extern crate tint;
+use tint::Color;
+
 extern crate chrono;
 use chrono::{NaiveDate, Duration};
 use chrono::format::ParseError;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use serde::{Deserializer, Serializer};
 use std::default::Default;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-#[derive(Serialize, Deserialize, Default)]
-pub struct ProductionPhase {
-    pub description: String,
+#[derive(Clone, Debug, PartialEq)]
+pub enum ResourceType {
+    Fermentor,
+    Kettle,
+    MashTun,
+    LauterTun,
+    Keg,
+    Kegerator,
+    Other(String)
+}
+
+impl<'de> Deserialize<'de> for ResourceType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de> {
+            let s = String::deserialize(deserializer)?;
+            Ok(match s.as_str() {
+                "fermentor" => ResourceType::Fermentor,
+                "kettle" => ResourceType::Kettle,
+                "mashtun" => ResourceType::MashTun,
+                "lautertun" => ResourceType::LauterTun,
+                "keg" => ResourceType::Keg,
+                "kegerator" => ResourceType::Kegerator,
+                _ => ResourceType::Other(s)
+            })
+    }
+}
+
+impl Serialize for ResourceType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer
+    {
+        serializer.serialize_str(match *self {
+            ResourceType::Fermentor => "fermentor",
+            ResourceType::Kettle => "kettle",
+            ResourceType::MashTun => "mashtun",
+            ResourceType::LauterTun => "lautertun",
+            ResourceType::Keg => "keg",
+            ResourceType::Kegerator => "kegerator",
+            ResourceType::Other(ref other) => other
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Resource {
     pub id: usize,
+    pub name: String,
+
+    #[serde(rename="type")]
+    pub resource_type: ResourceType,
+
+    #[serde(rename="capacity")]
+    pub capacity_str: String
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, PartialEq, Debug)]
+pub struct ProductionPhaseTemplate {
+    pub description: String,
+    pub id: String,
     pub order: usize,
 
     #[serde(rename="defaultDuration")]
@@ -20,17 +78,20 @@ pub struct ProductionPhase {
     default_duration: String
 }
 
-// pub struct Resource {
-//     pub id: usize,
-//     pub name: String,
-//     pub
-//
-// }
-
-impl ProductionPhase {
+impl ProductionPhaseTemplate {
     pub fn default_duration(&self) -> Option<Duration> {
         let mut characters: Vec<_> = self.default_duration.chars().collect();
-        match characters.pop() {
+        let mut identifier = None;
+        if characters.len() > 0 {
+            let last_character = characters[characters.len()- 1];
+            let is_last_character_digit = last_character.to_string().parse::<usize>().is_ok();
+            identifier = Some('d');
+            if !is_last_character_digit {
+                identifier = characters.pop();
+            }
+        }
+
+        match identifier {
             Some(x) => {
                 let digit_string: String = characters.into_iter().collect();
                 let digits: i64 = digit_string.parse::<i64>().unwrap();
@@ -39,7 +100,7 @@ impl ProductionPhase {
                     'w' => Some(Duration::weeks(digits)),
                     'd' => Some(Duration::days(digits)),
                     'h' => Some(Duration::hours(digits)),
-                    _ => Some(Duration::days(digits))
+                    _ => None
                 }
             },
             None => None
@@ -64,7 +125,19 @@ pub struct ProductionSchedule {
     pub name: String,
     pub id: usize,
     pub timeline: ProductionTimeline,
-    pub phases: Vec<ProductionPhase>
+
+    #[serde(rename="phaseTemplates")]
+    pub phase_templates: Vec<ProductionPhaseTemplate>,
+    pub resources: Vec<Resource>,
+
+    // #[serde(skip_serializing, skip_deserializing)]
+    // recipes: Vec<Recipe>,
+
+    #[serde(rename="recipes")]
+    pub recipe_specs: Vec<RecipeSpec>,
+
+    #[serde(skip_serializing, skip_deserializing)]
+    last_id_used: usize
 }
 
 impl ProductionSchedule {
@@ -74,12 +147,120 @@ impl ProductionSchedule {
         let mut contents: String = String::new();
         buf_reader.read_to_string(&mut contents).unwrap();
 
-        let result: Result<ProductionSchedule> = serde_json::from_str(contents.as_str());
+        let result: serde_json::Result<ProductionSchedule> = serde_json::from_str(contents.as_str());
         match result {
-            Ok(x) => x,
+            Ok(mut x) => {
+                x.last_id_used = 0;
+                x
+            },
             Err(e) => {
                 panic!("Unable to parse due to: {}", e);
             }
         }
     }
+
+    pub fn get_phase_by_id(&self, id: &str) -> Option<ProductionPhaseTemplate> {
+        for next_phase in &self.phase_templates {
+            if next_phase.id == id {
+                return Some(next_phase.clone());
+            }
+        }
+
+        None
+    }
+
+    pub fn get_resource_by_id(&self, id: usize) -> Option<Resource> {
+        for next_res in &self.resources {
+            if next_res.id == id {
+                return Some(next_res.clone());
+            }
+        }
+
+        None
+    }
+
+    // pub fn get_recipe_by_id(&mut self, id: usize) -> Option<Recipe> {
+    //     let mut spec: Option<RecipeSpec> = None;
+    //     let recipes = self.recipe_specs.clone();
+    //     for next_recipe in recipes {
+    //         if next_recipe.id == id {
+    //             spec = Some(next_recipe);
+    //             break;
+    //         }
+    //     }
+    //
+    //     match spec {
+    //         Some(x) => Some(Recipe {
+    //                      id: self.get_next_id(),
+    //                      name: x.name.clone(),
+    //                      color: x.color(),
+    //                      phases: vec![]
+    //                  }),
+    //         None => None
+    //     }
+    // }
+
+    pub fn get_recipe_by_name(&mut self, name: &str) -> Option<Recipe> {
+        let mut spec: Option<RecipeSpec> = None;
+
+        let recipes = self.recipe_specs.clone();
+        for next_recipe in recipes {
+            if next_recipe.name == name {
+                spec = Some(next_recipe);
+                break;
+            }
+        }
+
+        match spec {
+            Some(x) => Some(Recipe {
+                         id: self.get_next_id(),
+                         name: x.name.clone(),
+                         color: x.color(),
+                         phases: vec![]
+                     }),
+            None => None
+        }
+    }
+
+    fn get_next_id(&mut self) -> usize {
+        let ret_id: usize = self.last_id_used;
+        self.last_id_used = self.last_id_used + 1;
+
+        ret_id
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct PhaseInstanceSpec {
+    template: String
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct RecipeSpec {
+    pub name: String,
+
+    #[serde(rename="color")]
+    pub color_hex: String,
+
+    #[serde(rename="phases")]
+    pub phase_specs: Vec<PhaseInstanceSpec>
+}
+
+impl RecipeSpec {
+    pub fn color(&self) -> Color {
+        Color::from(&self.color_hex)
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct PhaseInstance {
+    duration: Duration
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Recipe {
+    pub id: usize,
+    pub name: String,
+    pub color: Color,
+    pub phases: Vec<PhaseInstance>
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -4,13 +4,18 @@
 // #[macro_use]
 // extern crate serde_json;
 
+extern crate tint;
+use tint::Color;
+
 use chrono::{NaiveDate, Duration};
 
 use chronogrog::ProductionSchedule;
+use chronogrog::ProductionPhaseTemplate;
+use chronogrog::ResourceType;
 
 #[test]
 fn it_should_load_a_json_file_into_a_new_production_schedule() {
-    let ps = ProductionSchedule::new("tests/fixtures/productionSchedule.json");
+    let mut ps = ProductionSchedule::new("tests/fixtures/productionSchedule.json");
 
     assert_eq!("Simple Production Schedule", ps.name);
     assert_eq!(1, ps.id);
@@ -20,7 +25,84 @@ fn it_should_load_a_json_file_into_a_new_production_schedule() {
 
     assert_eq!(expected_date, ps.timeline.start_date());
 
-    assert_eq!("Planning", ps.phases[0].description);
+    assert_eq!("Planning", ps.phase_templates[0].description);
 
-    assert_eq!(Some(Duration::hours(1)), ps.phases[0].default_duration());
+    assert_eq!(Some(Duration::hours(1)), ps.phase_templates[0].default_duration());
+
+    assert_eq!("Primary Fermentation", ps.phase_templates[2].description);
+    assert_eq!(Some(Duration::days(10)), ps.phase_templates[2].default_duration());
+
+    assert_eq!("Secondary Fermentation", ps.phase_templates[3].description);
+    assert_eq!(Some(Duration::weeks(4)), ps.phase_templates[3].default_duration());
+
+    assert_eq!(Some(ps.phase_templates[3].clone()), ps.get_phase_by_id("secondary"));
+
+    assert_eq!(6, ps.resources.len());
+
+    // There should be a kettle in the resources
+    let resources = &ps.resources;
+    let mut found = false;
+    for next in resources {
+        match next.resource_type {
+            ResourceType::Kettle => {
+                found = true;
+            }
+            _ => { found = found; }
+        };
+    }
+
+    assert!(found);
+
+    // Resource with id 1 should exist and be of type 'fermentor'
+    match &ps.get_resource_by_id(1) {
+        Some(x) => {
+            assert_eq!(ResourceType::Fermentor, x.resource_type);
+        },
+        None => { assert!(false) }
+    }
+
+    let damned_squirrel = ps.get_recipe_by_name("Damned Squirrel Mk. II").unwrap();
+    assert_eq!(damned_squirrel.name, "Damned Squirrel Mk. II");
+    assert_eq!(Color::from_rgb255(122, 86, 36), damned_squirrel.color);
+
+    // let damned_squirrel2 = ps.get_recipe_by_id(damned_squirrel.id).unwrap();
+    // assert_eq!(damned_squirrel, damned_squirrel2);
+}
+
+#[test]
+fn it_should_not_allow_for_an_empty_default_duration() {
+    let funny_prod_schedule_json = r#"
+        {
+            "id": "erroneous",
+            "description": "Erroneous Phase",
+            "order": 39182,
+            "defaultDuration": ""
+        }
+    "#;
+
+    let result: ProductionPhaseTemplate = serde_json::from_str(funny_prod_schedule_json).unwrap();
+
+    assert_eq!(None, result.default_duration());
+}
+
+#[test]
+fn it_should_reject_an_unknown_specifier_for_default_duration() {
+    let funny_prod_schedule_json = r#"
+        {
+            "id": "erroneous",
+            "description": "Erroneous Phase",
+            "order": 39182,
+            "defaultDuration": "25x"
+        }
+    "#;
+
+    let result: ProductionPhaseTemplate = serde_json::from_str(funny_prod_schedule_json).unwrap();
+
+    assert_eq!(None, result.default_duration());
+}
+
+#[test]
+#[should_panic]
+fn it_should_panic_on_an_unparseable_json_file() {
+    ProductionSchedule::new("tests/fixtures/bad_production_schedule.json");
 }
