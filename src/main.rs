e4db08c6ff2a80ff3b31af1e306f131ddaef8f0d
Scott Johnson <jaywir3@gmail.com>
2019-12-26 15:59:04 -0600

    :sparkles: Add command line arguments for input and output to main.rs
    Eventually, we'll need to take input from more than just standard input,
    and allow for output to more than just standard output. In order to
    accomplish this task, we need to make sure that the command line arguments
    for the binary version of chronogrog take options that allow the user to
    specify these arguments.
    
    Refs #2.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::io::{Read, Write};
+use std::fs::File;
+
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    // Take input in the form of JSON
+    let app_name = format!("{}", env!("CARGO_PKG_NAME"));
+    let app_description = format!("{}", env!("CARGO_PKG_DESCRIPTION"));
+    let authors = format!("{}", env!("CARGO_PKG_AUTHORS"));
+
+    let matches = App::new(app_name)
+      .version(crate_version!())
+      .about(&app_description[..])
+      .author(&authors[..])
+      .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("INPUT")
+                .help("Specify an input file to read from. Defaults to standard input.")
+                .takes_value(true),
+      )
+      .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("OUTPUT")
+                .help("Specify an output file to write to. Defaults to standard output.")
+                .takes_value(true),
+      )
+      .get_matches();
+
+    let input_file: Box<dyn Read> = match matches.value_of("input") {
+        Some(in_file) => match File::open(in_file) {
+                Ok(f) => Box::new(f),
+                Err(e) => {
+                    panic!(format!("{}: {}", e, in_file));
+                }
+        },
+        None => Box::new(std::io::stdin())
+    };
+
+    let output_file: Box<dyn Write> = match matches.value_of("output") {
+        Some(out_file) => match File::create(out_file) {
+            Ok(f) => Box::new(f),
+            Err(e) => panic!(format!("{}: {}", e, out_file))
+        },
+        None => Box::new(std::io::stdout())
+    };
+
+    // println!("Reading from stdin? {:?}", input_file == std::io::stdin());
+    // println!("Writing to stdout? {:?}", output_file == std::io::stdin());
 }
